3dc91552bce0293999635ba2df3ab2ed126d3a5c
Chrissy-coder <christine.soropa@gmail.com>
2023-10-01 12:26:39 +1300

    Created weather structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container"> 
-      <h1> Weather App </h1>
-     
-      <footer>
-        This open-source project was coded by Christine Soropa on 
-        <a
-          href="https://github.com/Chrissy-coder/app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-           Github
-        </a>
-      </footer>
-     </div> 
+      <div className="container">
+        <Weather />
+        <footer>
+          This open-source project was coded by Christine Soropa on
+          <a
+            href="https://github.com/Chrissy-coder/app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
