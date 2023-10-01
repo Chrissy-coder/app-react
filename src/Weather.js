3d511e3ee061e35fc2672a03ecef894ae187e37c
Chrissy-coder <christine.soropa@gmail.com>
2023-10-01 16:41:11 +1300

    Styled Weather  image and temp.

--- src/Weather.js
+++ src/Weather.js
@@ -28,13 +28,17 @@ export default function Weather() {
         <li>Sunday 12:08</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Cloudy"
-          />
-          6°C
+          <div className="d-flex">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly Cloudy"
+              className="float-left"
+            />
+            <span className="temperature">6</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
