91587cafc15ffdedf6bba6e0bd666707648ef00c
Chrissy-coder <christine.soropa@gmail.com>
2023-10-02 20:09:49 +1300

    Added axios, fixed CSS & API calls.

--- src/Weather.js
+++ src/Weather.js
@@ -1,65 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  let weatherData = {
-    city: "Auckland",
-    temperature: "12",
-    date: "Sunday 8:34",
-    description: "Cloudy",
-    img: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
-    humidity: 83,
-    wind: 2,
-  };
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: "Sunday 06:00",
+      description: response.data.weather[0].description,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
 
-  return (
-    <div className="Weather">
-      <form className="search-form" id="search-form">
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type a city.."
-              autofocus="on"
-              autocomplete="off"
-              id="city-input"
-              className="form-control shadow-sm"
-            />
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form className="search-form" id="search-form">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Type a city.."
+                autofocus="on"
+                autocomplete="off"
+                id="city-input"
+                className="form-control shadow-sm"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-secondary"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-secondary" />
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="d-flex">
+              <img
+                src={weatherData.iconUrl}
+                alt={weatherData.description}
+                className="float-left"
+              />
+              <span className="temperature">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="units">
+                °C │<a href="/">°F </a>
+              </span>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind:{weatherData.wind} km/h</li>
+            </ul>
           </div>
         </div>
-      </form>
-      <h1>{weatherData.city}</h1>
-      <ul>
-        <li>{weatherData.date}</li>
-        <li>{weatherData.description}</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <div className="d-flex">
-            <img
-              src={weatherData.img}
-              alt={weatherData.description}
-              className="float-left"
-            />
-            <span className="temperature">{weatherData.temperature}</span>
-            <span className="units">
-              °C │<a href="/">°F </a>
-            </span>
-          </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity: {weatherData.humidity}%</li>
-            <li>Wind:{weatherData.wind} km/h</li>
-          </ul>
-        </div>
+
+        <span className="line"></span>
+        <div className="weather-forecast" id="forecast"></div>
       </div>
+    );
+  } else {
+    const apiKey = "d04cd0ed2e3cbb22e88b9de7c048d2a3";
+    let city = "London";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
 
-      <span className="line"></span>
-      <div className="weather-forecast" id="forecast"></div>
-    </div>
-  );
+    return "Loading..";
+  }
 }
