d36e96119c8087421322ce0b702b67e51e1f3463
Chrissy-coder <christine.soropa@gmail.com>
2023-10-02 13:10:22 +1300

    Included weather temperature and weather icon

--- src/Weather.js
+++ src/Weather.js
@@ -11,13 +11,14 @@ export default function Weather(props) {
   function handleResponse(response) {
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
       wind: response.data.wind.speed,
       city: response.data.name,
-      date: new Date(response.data.dt * 1000),
-      humidity: response.data.temperature.humidity,
-      description: response.data.condition.description,
-      icon_url: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
     });
   }
 
