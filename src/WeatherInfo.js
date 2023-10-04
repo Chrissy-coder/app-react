4b3ce6b1630463911c06f07aa15c193f8f20c3e6
Chrissy-coder <christine.soropa@gmail.com>
2023-10-04 17:09:28 +1300

    Styled icon.

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -16,12 +16,9 @@
        <div className="row mt-3">
          <div className="col-6">
            <div className="d-flex">
-             <WeatherIcon code={props.data.icon} />
-             <img
-               src={props.iconUrl}
-               alt=''
-               className="float-left"
-             />
+             <div>
+               <WeatherIcon code={props.data.icon} size={52} />
+             </div>
              <span className="temperature">
                {Math.round(props.data.temperature)}
              </span>
